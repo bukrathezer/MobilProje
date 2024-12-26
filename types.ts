1c2d1e9224448ede797b2df5ce022a83446ebc94
Burak_Fidan <103516974+FBurak@users.noreply.github.com>
2024-12-26 20:47:27 +0300

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,7 +1,1 @@
-export type ParamList = {
-    AdminDashboard: undefined;
-    EditTests: { userId: string };
-    ViewTests: { userId: string }; // ViewTests için userId parametresi tanımlandı
-    Login: undefined;
-    SignUp: undefined;
-};
+
