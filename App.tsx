8a189990ba5df25d4873e39da1f9967375a539c6
bukrathezer <104260118+bukrathezer@users.noreply.github.com>
2024-12-26 23:21:47 +0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -31,7 +31,7 @@ export default function App() {
                 };
             }
         } catch (error) {
-            console.error('Error fetching user info:', error);
+            console.error('Error fetching user info: ', error);
         }
         return null;
     };
