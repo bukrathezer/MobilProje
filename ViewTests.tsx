ae55d048040486f7950e1e7cbc3d8cda7ca60846
Burak_Fidan <103516974+FBurak@users.noreply.github.com>
2024-12-28 15:18:32 +0300

    Update ViewTests.tsx

--- ViewTests.tsx
+++ ViewTests.tsx
@@ -1,8 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, StyleSheet, FlatList, TextInput, Alert } from 'react-native';
+import { View, Text, Button, StyleSheet, FlatList, TextInput, Alert, Modal, ScrollView } from 'react-native';
 import { FIRESTORE_DB, FIREBASE_AUTH } from '../../FirebaseConfig';
-import { doc, getDoc, updateDoc } from 'firebase/firestore';
+import { doc, getDoc, updateDoc, getDocs, collection, Timestamp } from 'firebase/firestore';
 import { signOut, updatePassword } from 'firebase/auth';
+
+interface Tests {
+    timestamp: number;
+    IgA?: number | null;
+    IgG?: number | null;
+    IgG1?: number | null;
+    IgG2?: number | null;
+    IgG3?: number | null;
+    IgG4?: number | null;
+    IgM?: number | null;
+}
 
 interface ViewTestsProps {
     userId: string;
@@ -11,18 +22,13 @@ interface ViewTestsProps {
 }
 
 const ViewTests = ({ userId, firstName, lastName }: ViewTestsProps) => {
-    const [tests, setTests] = useState<Record<string, number | null>>({});
+    const [tests, setTests] = useState<Tests[]>([]);
     const [loading, setLoading] = useState(true);
     const [newPassword, setNewPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
-    const [userInfo, setUserInfo] = useState<{
-        email: string;
-        age: number | null;
-        firstName: string;
-        lastName: string;
-    }>({
+    const [userInfo, setUserInfo] = useState({
         email: '',
-        age: null,
+        age: null as number | null,
         firstName: '',
         lastName: '',
     });
@@ -77,29 +83,52 @@ const ViewTests = ({ userId, firstName, lastName }: ViewTestsProps) => {
         }
     };
 
-    useEffect(() => {
-        const fetchUserData = async () => {
-            try {
-                const userDoc = await getDoc(doc(FIRESTORE_DB, 'users', userId));
-                if (userDoc.exists()) {
-                    const data = userDoc.data();
-                    setUserInfo({
-                        firstName: data.firstName || '',
-                        lastName: data.lastName || '',
-                        email: data.email || '',
-                        age: data.age || null,
-                    });
-                    setTests(data.tests || {});
-                }
-            } catch (error) {
-                console.error('Error fetching user data:', error);
-            } finally {
-                setLoading(false);
+    const fetchUserData = async () => {
+        try {
+            const userDoc = await getDoc(doc(FIRESTORE_DB, 'users', userId));
+            if (userDoc.exists()) {
+                const data = userDoc.data();
+                setUserInfo({
+                    firstName: data.firstName || '',
+                    lastName: data.lastName || '',
+                    email: data.email || '',
+                    age: data.age || '' ,
+                });
             }
-        };
+        } catch (error) {
+            console.error('Error fetching user data:', error);
+        } finally {
+            setLoading(false);
+        }
+    };
 
+    
+
+const fetchTestsForUser = async () => {
+    try {
+        const testsSnapshot = await getDocs(collection(FIRESTORE_DB, `users/${userId}/tests`));
+        const testsList: Tests[] = testsSnapshot.docs.map((doc) => {
+            const data = doc.data();
+            if (data.timestamp && data.timestamp instanceof Timestamp) {
+                data.timestamp = data.timestamp.toDate().getTime(); // Timestamp'i milisaniye dönüştür
+            }
+            return data as Tests;
+        });
+        setTests(testsList);
+    } catch (error) {
+        console.error('Error fetching tests:', error);
+        Alert.alert('Error', 'Veri çekme sırasında bir hata oluştu.');
+    }
+};
+
+    const formatTimestamp = (timestamp: number) => {
+        const date = new Date(timestamp);
+        return date.toLocaleString();
+    };
+
+    useEffect(() => {
         fetchUserData();
-    }, [userId]);
+    }, []);
 
     if (loading) {
         return (
@@ -183,26 +212,38 @@ const ViewTests = ({ userId, firstName, lastName }: ViewTestsProps) => {
                 onPress={() => {
                     setShowAnalyses(true);
                     setShowUserInfo(false);
+                    fetchTestsForUser();
                 }}
             />
 
             {showAnalyses && (
-                <FlatList
-                    data={Object.entries(tests)}
-                    keyExtractor={([key]) => key}
-                    renderItem={({ item }) => (
-                        <View style={styles.testRow}>
-                            <Text style={styles.testKey}>{item[0]}</Text>
-                            <Text style={styles.testValue}>{item[1] !== null ? item[1].toString() : 'N/A'}</Text>
-                        </View>
-                    )}
-                    ListHeaderComponent={() => (
-                        <View style={styles.tableHeader}>
-                            <Text style={styles.tableHeaderText}>Analysis Name</Text>
-                            <Text style={styles.tableHeaderText}>Value</Text>
-                        </View>
-                    )}
-                />
+                <Modal visible={showAnalyses} animationType="slide">
+                    <ScrollView style={styles.scrollViewContainer}>
+                        <Button
+                            title="Close"
+                            onPress={() => setShowAnalyses(false)}
+                            color="red"
+                        />
+                        {tests.map((test, index) => (
+                            <View key={index} style={styles.tableContainer}>
+                                <Text style={styles.timestamp}>
+                                    Timestamp: {formatTimestamp(test.timestamp)}
+                                </Text>
+                                <View style={styles.tableHeader}>
+                                    <Text style={styles.tableCell}>Test Type</Text>
+                                    <Text style={styles.tableCell}>Value</Text>
+                                </View>
+                                <View style={styles.tableRow}><Text style={styles.tableCell}>IgA</Text><Text style={styles.tableCell}>{test.IgA ?? 'N/A'}</Text></View>
+                                <View style={styles.tableRow}><Text style={styles.tableCell}>IgG</Text><Text style={styles.tableCell}>{test.IgG ?? 'N/A'}</Text></View>
+                                <View style={styles.tableRow}><Text style={styles.tableCell}>IgG1</Text><Text style={styles.tableCell}>{test.IgG1 ?? 'N/A'}</Text></View>
+                                <View style={styles.tableRow}><Text style={styles.tableCell}>IgG2</Text><Text style={styles.tableCell}>{test.IgG2 ?? 'N/A'}</Text></View>
+                                <View style={styles.tableRow}><Text style={styles.tableCell}>IgG3</Text><Text style={styles.tableCell}>{test.IgG3 ?? 'N/A'}</Text></View>
+                                <View style={styles.tableRow}><Text style={styles.tableCell}>IgG4</Text><Text style={styles.tableCell}>{test.IgG4 ?? 'N/A'}</Text></View>
+                                <View style={styles.tableRow}><Text style={styles.tableCell}>IgM</Text><Text style={styles.tableCell}>{test.IgM ?? 'N/A'}</Text></View>
+                            </View>
+                        ))}
+                    </ScrollView>
+                </Modal>
             )}
         </View>
     );
@@ -210,6 +251,11 @@ const ViewTests = ({ userId, firstName, lastName }: ViewTestsProps) => {
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+        padding: 10,
+        backgroundColor: '#f8f9fa',
+    },
+    scrollViewContainer: {
         flex: 1,
         padding: 10,
         backgroundColor: '#f8f9fa',
@@ -240,6 +286,19 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         marginBottom: 10,
     },
+    tableContainer: {
+        marginBottom: 20,
+        borderWidth: 1,
+        borderColor: '#ddd',
+        borderRadius: 5,
+        padding: 10,
+        backgroundColor: '#fff',
+    },
+    timestamp: {
+        fontSize: 14,
+        color: '#777',
+        marginBottom: 10,
+    },
     tableHeader: {
         flexDirection: 'row',
         justifyContent: 'space-between',
@@ -250,27 +309,18 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         marginBottom: 5,
     },
-    tableHeaderText: {
+    tableCell: {
         fontSize: 16,
         fontWeight: 'bold',
+        flex: 1,
+        textAlign: 'center',
     },
-    testRow: {
+    tableRow: {
         flexDirection: 'row',
         justifyContent: 'space-between',
         padding: 10,
-        backgroundColor: '#fff',
         borderBottomWidth: 1,
         borderColor: '#ddd',
-        borderRadius: 5,
-        marginVertical: 5,
-    },
-    testKey: {
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
-    testValue: {
-        fontSize: 16,
-        color: '#666',
     },
     loadingContainer: {
         flex: 1,
