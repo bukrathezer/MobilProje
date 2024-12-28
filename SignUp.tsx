c0e8a199200c0eec99342384893bf8ee4a769199
bukrathezer <104260118+bukrathezer@users.noreply.github.com>
2024-12-28 19:56:01 +0300

    Update SignUp.tsx

--- SignUp.tsx
+++ SignUp.tsx
@@ -14,18 +14,21 @@ const SignUp = ({ navigation }: SignUpProps) => {
     const [password, setPassword] = useState('');
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
-    const [age, setAge] = useState('');
+    const [ageYears, setAgeYears] = useState('');
+    const [ageMonths, setAgeMonths] = useState('');
     const [loading, setLoading] = useState(false);
 
     const signUp = async () => {
         setLoading(true);
         try {
+            const totalAgeInMonths = parseInt(ageYears, 10) * 12 + parseInt(ageMonths, 10);
+
             const response = await createUserWithEmailAndPassword(FIREBASE_AUTH, email, password);
             await setDoc(doc(FIRESTORE_DB, 'users', response.user.uid), {
                 email: response.user.email,
                 firstName,
                 lastName,
-                age: parseInt(age, 10), // Store age as an integer
+                ageInMonths: totalAgeInMonths, // Yaş bilgisi ay olarak kaydediliyor
                 role: 'user',
                 tests: {
                     IgA: 0,
@@ -79,13 +82,24 @@ const SignUp = ({ navigation }: SignUpProps) => {
                     autoCapitalize="none"
                     onChangeText={(text) => setPassword(text)}
                 />
-                <TextInput
-                    value={age}
-                    style={styles.input}
-                    placeholder="Age"
-                    keyboardType="numeric"
-                    onChangeText={(text) => setAge(text)}
-                />
+                <View style={styles.ageContainer}>
+                    <TextInput
+                        value={ageYears}
+                        style={styles.ageInput}
+                        placeholder="Years"
+                        keyboardType="numeric"
+                        onChangeText={(text) => setAgeYears(text)}
+                    />
+                    <Text style={styles.ageSeparator}>Years</Text>
+                    <TextInput
+                        value={ageMonths}
+                        style={styles.ageInput}
+                        placeholder="Months"
+                        keyboardType="numeric"
+                        onChangeText={(text) => setAgeMonths(text)}
+                    />
+                    <Text style={styles.ageSeparator}>Months</Text>
+                </View>
                 {loading ? (
                     <ActivityIndicator size="large" color="blue" />
                 ) : (
@@ -111,5 +125,23 @@ const styles = StyleSheet.create({
         borderRadius: 4,
         padding: 10,
         backgroundColor: '#fff',
+    },
+    ageContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginVertical: 10,
+    },
+    ageInput: {
+        height: 50,
+        borderWidth: 1,
+        borderRadius: 4,
+        padding: 10,
+        backgroundColor: '#fff',
+        width: '40%',
+    },
+    ageSeparator: {
+        fontSize: 16,
+        marginHorizontal: 5,
     },
 });
